bed6e4c8beb84755a01e07b2cef957c31cf252df
unknown <12163866@inha.edu>
2023-11-28 20:18:37 +0900

    login/logput

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPenFill } from "react-icons/bs";
-import { login } from "../api/firebase";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
-export default function Navbar(){
-    return (
-        <header className="flex justify-between border-b border-gray-300 p-2">
-            <Link to ='/' className="flex items-center text-4xl text-brand">
-                <FiShoppingBag/>
-                <h1>Shoppy</h1>
-            </Link>
-            <nav className="flex items-center gap-4 font-semibold">
-                <Link to='/products'>Products</Link>
-                <Link to='/carts'>carts</Link>
-                <Link to='/products/new' className="text-2xl">
-                    <BsFillPenFill/>
-                </Link>
-                <button onClick={login}>Login</button>
-            </nav>
-        </header>
-    )
+export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">carts</Link>
+        <Link to="/products/new" className="text-2xl">
+          <BsFillPenFill />
+        </Link>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
+      </nav>
+    </header>
+  );
 }
